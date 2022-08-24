ce05196d96d33cc099a25e15fe38304869e16e8d
Александр Сапрыкин <alamana13@mail.ru>
2022-08-24 13:51:21 +0300

    Сделал работающие 3 функции. Проверка на float нужна потому что иногда рейтинга нет и можно было и на пустоту проверить, но я решил на float. Функция request делает запрос к imdb и возвращает уже python объект который уже дальше обрабатывает функция popular. А popular красиво всё это выводит в консоль, попутно сортируя фильмы с низким рейтингом рейтингом, малым кол-вом голосов и годом до 2018. Дальше надо будет докрутить сюда отправку по email и желаетельно еще трейлер-тизер фильма впихнуть

--- main.py
+++ main.py
@@ -1,27 +1,72 @@
-# This is a sample Python script.
+import time
+import requests
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def request():
+    url = "https://imdb-api.com/en/API/MostPopularMovies/k_1cob5uy7"
+    payload = {}
+    headers = {}
+    # Создаем объект response с json внутри
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # Далее происходит перевод текста в кодировку
+    json_data = response.text.encode('utf8')
+
+    # Дальше мы создаем pytjon объект из изначального json
+    json_object = json.loads(json_data)
+
+    # #Дальнейшая строчка уже не нужна, но так для понимания - это красивое отображение json столбиков в виде str
+    # json_formatted_str = json.dumps(json_object, indent=1)
+
+    # Эти два цикла как раз печатают все фильмы из запроса по условию
+    # y = json.loads(json_data)
+    return json_object['items']
+
+
+def popular():
+    y = request()
+    count = 1
+    for i in range(1, len(y)):
+        if isfloat(y[i]['imDbRating']):
+            try:
+                if float(y[i]['imDbRating']) > 5.5 and int(y[i]['imDbRatingCount']) > 50000 and int(
+                        y[i]['year']) > 2018:
+                    print(f'№{count}')
+                    for key, value in y[i].items():
+                        if key not in ['id', 'rankUpDown', 'rank', 'title']:
+                            print(f'{key} ---> {value}')
+                    print()
+                    count += 1
+            except Exception as e:
+                print(f"something went wrong! ---> {e} ")
+                time.sleep(5)
+                continue
+        else:
+            continue
+
+
+# API key k_1cob5uy7
 
 
 """Код ниже отправляет email. Я создал ящик на gmail чтобы отправлять всякое. Мне понадобится отправлять письмо в 
 определенном формате чтобы это выглядело классно. Т е постер фильма, каст, актеры и т.п."""
-import yagmail
 
-yag = yagmail.SMTP(user='tet.yag2022', password='jmzbgylqzquygkih')
-context = ['Привет это 1 тестовое сообщение']
-yag.send(to='alamana13@mail.ru', subject='test', contents=context)
-
-
+# yag = yagmail.SMTP(user='tet.yag2022', password='jmzbgylqzquygkih')
+# context = ['Привет это 1 тестовое сообщение']
+# yag.send(to='alamana13@mail.ru', subject='test', contents=context)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    popular()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
