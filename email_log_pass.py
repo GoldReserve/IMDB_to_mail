c05152922b8b891cbba7adb2d3436e0d94e22e01
Alexander <100153293+GoldReserve@users.noreply.github.com>
2022-09-13 13:59:24 +0300

    Update email_log_pass.py

--- email_log_pass.py
+++ email_log_pass.py
@@ -1,8 +1,8 @@
 # Прописываем email с которого будем отправлять фильм
-sender_email: str = 'tet.yag2022'
+sender_email: str = ''# если ящик 1234@gmail.com пишем 1234
 
 # Прописываем полученный пароль приложения
-sender_app_pass: str = 'jmzbgylqzquygkih'
+sender_app_pass: str = ''
 
 # Прописываем email получателя
-receive_email: str = 'alamana13@mail.ru'
+receive_email: str = 'alamana13@mail.ru'# если почта 1234@gmail.com так и пишем '1234@gmail.com'
